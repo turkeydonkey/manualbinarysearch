f8dfbdae977ab99b5292b5d569bbb6bcacf68cef
turkeydonkey <iboggs@gmail.com>
2020-05-01 03:58:30 -0700

    add powershell version

---
+++ manualbinarysearch.ps1
@@ -0,0 +1,22 @@
+﻿$minimum = 1
+$maximum = 100
+$number = Get-Random -Minimum 1 -Maximum 100
+$guess = ""
+
+while (($guess -as [int]) -ne $null -and ($guess -lt $minimum -or $guess -gt $maximum)) {
+    Write-Output "I'm thinking of a number between $minimum and $maximum."
+    $guess = (Read-Host -Prompt "Enter a number") -as [int]
+}
+
+while (($guess -as [int]) -ne $number) {
+    Write-Output $number
+    if ($guess -lt $number) {
+        Write-Output "$guess is too low, try again"
+    }
+    elseif ($guess -gt $number) {
+        Write-Output "$guess is too high, try again"
+    }
+    $guess = (Read-Host -Prompt "Enter a number") -as [int]
+}
+
+Write-Output "$guess is correct!"
